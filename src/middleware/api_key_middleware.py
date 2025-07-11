dffdd6f33fb060bda535b941bb4b718e1d45e9b3
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-07-11 09:39:25 -0400

    Middleware now only blocks the /scrape-recipe and /merge-ingredients endpoints to allow access to docs without an api key

--- src/middleware/api_key_middleware.py
+++ src/middleware/api_key_middleware.py
@@ -8,7 +8,12 @@ class APIKeyMiddleware(BaseHTTPMiddleware):
     Middleware to check the api key
     """
 
+    PROTECTED_ROUTES = ["/scrape-recipe", "/merge-ingredients"]
+
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
+        if not any(request.url.path.startswith(path) for path in self.PROTECTED_ROUTES):
+            return await call_next(request)
+
         api_key = request.headers.get("X-API-Key")
         if api_key != os.getenv("ALLOWED_API_KEY"):  # Replace with your actual API key
             raise HTTPException(status_code=403, detail="Forbidden: Invalid API Key")
